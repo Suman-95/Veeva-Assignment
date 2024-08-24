c4ad710d40a8cc66bb5d6194737eb09316670f85
suman <sumanrajnayak@gmail.com>
2024-08-24 23:17:23 +0530

    prototype test

--- src/main/java/pagePackage/CPHomePage.java
+++ src/main/java/pagePackage/CPHomePage.java
@@ -15,9 +15,13 @@ public class CPHomePage extends CommonMethods {
     }
 @FindBy(xpath = "//img[@alt='NBA Logo']")
     WebElement nbaLogo;
-
+@FindBy(xpath = "//div[text()='x']")
+WebElement closeButtonPresale;
 
     public void click_logo(){
         click_on_element(nbaLogo);
+    }
+    public void close_presale_notification(){
+        click_on_element(closeButtonPresale);
     }
 }
--- src/test/java/stepdefinitions/StepDefinitions.java
+++ src/test/java/stepdefinitions/StepDefinitions.java
@@ -13,39 +13,12 @@ public class StepDefinitions {
     WebDriver driver=DriverHandler.getDriver();
     CommonMethods cm=new CommonMethods(driver);
     CPHomePage cph=new CPHomePage(driver);
-    /*@Given("I load the page")
-    public void i_load_the_page() {
-       *//* ChromeOptions options = new ChromeOptions();
-        options.addArguments("headless");*//*
-        driver = new ChromeDriver();
-        driver.manage().window().maximize();
-        driver.get("https://www.demoblaze.com/");
-        driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
-    }*/
-    @When("I see the page")
-    public void i_see_the_page() {
-        // Write code here that turns the phrase above into concrete actions
-        System.out.println("see the page");
-//        JavascriptExecutor js = (JavascriptExecutor) driver;
-//             js.executeScript("window.scrollBy(0,350)", "");
-        driver.findElement(By.linkText("Phones")).click();
-    }
-    @When("I click on the link")
-    public void i_click_on_the_link() {
-        // Write code here that turns the phrase above into concrete actions
-        System.out.println("click the link");
-    }
-    @Then("I verify the option")
-    public void i_verify_the_option() {
-        //String text = driver.findElement(By.xpath("//a[text()='Git Pocket Guide']")).getText();
-        //System.out.println(text);
-        driver.quit();
-    }
 
     @Given("I load the {string} page")
     public void iLoadThePage(String arg0) throws InterruptedException {
         cm.go_to_url(arg0);
-        Thread.sleep(5000);
+        Thread.sleep(1000);
+        cph.close_presale_notification();
         cph.click_logo();
     }
 }
