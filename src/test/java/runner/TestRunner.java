6e20c84779c8dd088784552e75fd5f0c21a46ea0
suman <sumanrajnayak@gmail.com>
2024-08-25 00:50:54 +0530

    Prototype in progress

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -2,13 +2,15 @@ package runner;
 
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
+import io.qameta.allure.testng.AllureTestNg;
+import org.testng.annotations.Listeners;
 
 @CucumberOptions(
         features = "src/test/java/features/",
         glue = {"stepdefinitions"},
         monochrome = true
 )
-
+@Listeners({AllureTestNg.class})
 public class TestRunner extends AbstractTestNGCucumberTests {
 
 }
